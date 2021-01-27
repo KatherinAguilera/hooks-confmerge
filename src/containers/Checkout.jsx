80bcbe386e573d3e26adbb338c867db19b4edadb
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2021-01-27 14:22:06 -0500

    REact helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import { Helmet } from 'react-helmet';
+
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
@@ -18,33 +20,58 @@ const Checkout = () => {
   }
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
-        {cart.map((item) => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>
-                $
-                {item.price}
-              </span>
+    <>
+      <Helmet>
+        <title>Lista de Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TU_USER" />
+        <meta name="twitter:creator" content="@TU_USER" />
+        <meta name="twitter:title" content="Conf Store" />
+        <meta name="twitter:description" content=" Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content=" Conf Store" />
+        <meta property="og:description" content=" Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzistore.xyz" />
+        <meta property="og:site_name" content=" Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
+          {cart.map((item) => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>
+                  $
+                  {item.price}
+                </span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                <i className="fas fa-trash-alt" />
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => (
-  <Products products={initialState.products} />
+  <>
+    <Helmet>
+      <title>Conf Productos</title>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@TU_USER" />
+      <meta name="twitter:creator" content="@TU_USER" />
+      <meta name="twitter:title" content="Conf Store" />
+      <meta name="twitter:description" content=" Conf Store" />
+      <meta
+        name="twitter:image"
+        content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+      />
+      <meta property="og:title" content=" Conf Store" />
+      <meta property="og:description" content=" Conf Store" />
+      <meta
+        property="og:image"
+        content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+      />
+      <meta property="og:url" content="platzistore.xyz" />
+      <meta property="og:site_name" content=" Conf Store" />
+      <meta property="og:locale" content="es_ES" />
+      <meta property="og:type" content="article" />
+      <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+    </Helmet>
+    <Products products={initialState.products} />
+  </>
   )
 
 export default Home;
